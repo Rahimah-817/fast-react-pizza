5c8062cc0ca835036dac0ca5ce077666e490e95d
Rahimah Ansari <rahimeansari7@gmail.com>
2024-06-30 09:38:37 +0430

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <Link tp='/'>Fast React Pizza Co.</Link>
+                <p>Rahimah</p>
+          </header>
+
+        </div>
+    )
+}
+
+export default Header
